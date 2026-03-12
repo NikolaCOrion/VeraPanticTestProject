7da7686b5382e39bca4f96bf68c8056fb208c41b
Nikola Curcic <ncurcic@gsmorg.com>
2026-03-12 11:54:04 +0100

    added a new task

--- app/main.py
+++ app/main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+'''
+create a new endpoint: /users/
+and create a list of hardcoded user objects
+each one should have properties:
+id
+name
+age
+'''
+
+
 @app.get("/my-first-endpoint/")
 def my_first_endpoint():
     return {"hello": "world"}
