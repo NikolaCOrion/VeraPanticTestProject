c31c951b46fa4b7ed3a147c6cb9964bac514a93a
Nikola Curcic <ncurcic@gsmorg.com>
2026-03-19 08:27:27 +0100

    added setup

--- app/src/models/models.py
+++ app/src/models/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy.orm import declarative_base, relationship
 
 
 Base = declarative_base()
@@ -11,3 +11,15 @@ class Profile(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     age = Column(Float, nullable=False)
+
+
+class Project(Base):
+    __tablename__ = "project"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=True)
+    profile_id = Column(Integer, ForeignKey('profile.id'), nullable=False)
+    start_date = Column(DateTime, nullable=True)
+    end_date = Column(DateTime, nullable=True)
+
+    profile = relationship("Profile", back_populates="projects")
